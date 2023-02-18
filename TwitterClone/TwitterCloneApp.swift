7bcdfd5a5c30697b94a4763ebb8bd5535a1a6cf7
Mandeep Singh <mdeep14293@gmail.com>
2023-02-18 21:13:55 +0530

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Deep Baath on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
